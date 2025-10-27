e344d1b1da89ec0c573e0c04caf67f040b912ea3
Honphan <honphan194@gmail.com>
2025-10-27 18:17:13 +0700

    fix: .env

--- FrontEnd/online-banking-frontend/vite.config.js
+++ FrontEnd/online-banking-frontend/vite.config.js
@@ -1,11 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: import.meta.env.VITE_BASE_PATH || '/online-banking-frontend/',
-  define: {
-    global: {},
-  },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
+  
+  return {
+    plugins: [react()],
+    base: env.VITE_BASE_PATH || '/online-banking-frontend/',
+    define: {
+      global: {},
+    },
+  }
 })
